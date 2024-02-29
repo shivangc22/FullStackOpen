ebcb9e61430113deab2a3a2ca0752e8e507c368f
Shivang Chopra <shivang.c@xebo.ai>
2024-02-29 11:23:26 +0530

    Solutions for exercises 1.6 and 1.7

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const onGoodFeedback = () => setGood(good + 1);
+  const onNeutralFeedback = () => setNeutral(neutral + 1);
+  const onBadFeedback = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>Give feedback</h1><br />
+      <Button onClick = {onGoodFeedback} label = 'Good' />
+      <Button onClick = {onNeutralFeedback} label = 'Neutral' />
+      <Button onClick = {onBadFeedback} label = 'Bad' /> <br />
+      <h1>Statistics</h1>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+    </div>
+  )
+}
+
+const Button = ({onClick, label}) => <button onClick = {onClick}>{label}</button>
+
+const StatisticLine = ({label, value}) => <p>{label}: {value}</p>
+
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <StatisticLine label = 'Good' value = {good} />
+      <StatisticLine label = 'Neutral' value = {neutral} />
+      <StatisticLine label = 'Bad' value = {bad} />
+      <StatisticLine label = 'All' value = {good + bad + neutral} />
+      <StatisticLine label = 'Average' value = {(good - bad)/(good + bad + neutral)} />
+      <StatisticLine label = 'Positive (in %)' value = {(good - bad) * 100/(good + bad + neutral)} />
+    </div>
+  )
+}
+export default App

945f42cb5825bedfa7084060774225a0a43e58c0
Shivang Chopra <shivang.c@xebo.ai>
2024-02-29 16:45:26 +0530

    Solution for exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const newNameObject = { name: newName }
+    setPersons(persons.concat(newNameObject))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {addNewName}>
+        <div>
+          name: <input value = {newName} onChange = {handleNewName}/>
+        </div>
+        <div>
+          <br /><button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Names</h2>
+      <StoredNames persons = {persons} />
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+const StoredNames = ({persons}) => {
+  return(
+    <div>
+      { persons.map(element => 
+      <ShowData key = {element.name} value = {element.name} />
+      )}
+    </div>
+  )
+}
+
+const ShowData = ({value}) => <p>{value}</p>
+export default App

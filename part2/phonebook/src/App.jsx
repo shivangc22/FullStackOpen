40c4fa08bdd75252004d1d44926ac49111a414b5
Shivang Chopra <shivang.c@xebo.ai>
2024-03-04 21:00:41 +0530

    Solutuion for exercise 2.6 - 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,50 +1,82 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: '98-76-543210'},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
 
-  const addNewName = (event) => {
-    event.preventDefault()
-    if(persons.some(person => person.name === newName.trim())) {
-      alert(`${newName} is already added to the phonebook`)
-    } else {
-      const newNameObject = { name: newName }
-      setPersons(persons.concat(newNameObject))
-      setNewName('')
-      }
-    }
+  const [newSearch, setNewSearch] = useState('')
 
-  const handleNewName = (event) => setNewName(event.target.value)
+  const handleSearchFilter = (event) => setNewSearch(event.target.value.toLowerCase())
+
+  const filteredResults = persons.filter(person =>
+    person.name.toLowerCase().includes(newSearch) || person.number.includes(newSearch)) 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {addNewName}>
-        <div>
-          name: <input value = {newName} onChange = {handleNewName}/>
-        </div>
-        <div>
-          <br /><button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Names</h2>
-      <StoredNames persons = {persons} />
+      <Filter newSearch = {newSearch} handleSearchFilter = {handleSearchFilter} />
+      <h2>Add a new entry</h2>
+      <FormDetails persons = {persons} setPersons = {setPersons} />
       <h2>Numbers</h2>
-      ...
+      <ShowEntries filteredResults = {filteredResults} />
     </div>
   )
 }
 
-const StoredNames = ({persons}) => {
+const Filter = ({newSearch, handleSearchFilter}) => {
   return(
-    <div>
-      { persons.map(element => 
-      <ShowData key = {element.name} value = {element.name} />
-      )}
-    </div>
+    <p>
+      Filter by: <input value = {newSearch} onChange = {handleSearchFilter} />
+    </p>
   )
 }
 
-const ShowData = ({value}) => <p>{value}</p>
+const FormDetails = ({persons, setPersons}) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
+
+  const addNewEntry = (event) => {
+    event.preventDefault();
+    if (persons.some(obj => obj.name === newName || obj.number === newNumber)) {
+      const existingEntry = persons.find(person => person.name === newName || person.number === newNumber);
+      if (existingEntry.name === newName) {
+        alert(`${newName} is already added to the phonebook, with the number ${existingEntry.number}`);
+      } else {
+        alert(`${newNumber} is already added to the phonebook, with the name ${existingEntry.name}`);
+      }
+    } else {
+      const newObject = { id: persons.length + 1, name: newName, number: newNumber };
+      setPersons(persons.concat(newObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+  return(
+    <form onSubmit = {addNewEntry}>
+      <div>
+        <p>name: <input value = {newName} onChange = {handleNewName}/></p>
+        <p>number: <input value = {newNumber} onChange = {handleNewNumber}/></p>
+      </div>
+      <div>
+        <br /><button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const ShowEntries = ({filteredResults}) => {
+  return(
+    <ol>
+      { filteredResults.map(element => <li key = {element.id}><ShowData name = {element.name} number = {element.number} /></li>)}
+    </ol>
+  )
+}
+
+const ShowData = ({name, number}) => <p>{name}: {number}</p>
 export default App
